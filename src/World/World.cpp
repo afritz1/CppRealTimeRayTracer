f746edc6ceaacfdd6df81ca291025dcfe0f98165
Aaron <afritz1@users.noreply.github.com>
2019-02-15 09:17:55 -0800

    Added members to World class.
    Currently it only allows for adding shapes and not removing them.

--- src/World/World.cpp
+++ src/World/World.cpp
@@ -1,2 +1,57 @@
 #include "World.h"
 
+int World::getRectCount() const
+{
+	return static_cast<int>(this->rects.size());
+}
+
+int World::getTriangleCount() const
+{
+	return static_cast<int>(this->triangles.size());
+}
+
+const Rect &World::getRect(int index) const
+{
+	return this->rects.at(index);
+}
+
+const Triangle &World::getTriangle(int index) const
+{
+	return this->triangles.at(index);
+}
+
+const Double3 &World::getFogColor() const
+{
+	return this->fogColor;
+}
+
+double World::getFogDistance() const
+{
+	return this->fogDistance;
+}
+
+void World::addRect(Rect &&rect)
+{
+	this->rects.push_back(std::move(rect));
+}
+
+void World::addTriangle(Triangle &&triangle)
+{
+	this->triangles.push_back(std::move(triangle));
+}
+
+void World::setFogColor(const Double3 &fogColor)
+{
+	this->fogColor = fogColor;
+}
+
+void World::setFogDistance(double fogDistance)
+{
+	this->fogDistance = fogDistance;
+}
+
+void World::init(const Double3 &fogColor, double fogDistance)
+{
+	this->setFogColor(fogColor);
+	this->setFogDistance(fogDistance);
+}
--- src/World/World.h
+++ src/World/World.h
@@ -1,9 +1,32 @@
 #ifndef RT_WORLD_H
 #define RT_WORLD_H
 
+#include <vector>
+
+#include "../Shapes/Rect.h"
+#include "../Shapes/Triangle.h"
+
 class World
 {
+private:
+	std::vector<Rect> rects;
+	std::vector<Triangle> triangles;
+	Double3 fogColor;
+	double fogDistance;
+public:
+	int getRectCount() const;
+	int getTriangleCount() const;
+	const Rect &getRect(int index) const;
+	const Triangle &getTriangle(int index) const;
+	const Double3 &getFogColor() const;
+	double getFogDistance() const;
 
+	void addRect(Rect &&rect);
+	void addTriangle(Triangle &&triangle);
+	void setFogColor(const Double3 &fogColor);
+	void setFogDistance(double fogDistance);
+
+	void init(const Double3 &fogColor, double fogDistance);
 };
 
 #endif
